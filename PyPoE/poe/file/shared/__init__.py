e7bde5886cbdf393bc514f9dd7152c8f67515273
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2025-06-14 13:58:13 +0700

    Log repeated http errors

--- PyPoE/poe/file/shared/__init__.py
+++ PyPoE/poe/file/shared/__init__.py
@@ -77,6 +77,8 @@ from time import sleep
 from typing import TYPE_CHECKING, Any, Callable, Dict, List, Union
 from urllib import parse, request
 
+from PyPoE.cli.core import Msg, console
+
 if TYPE_CHECKING:
     from PyPoE.poe.file.file_system import FileSystemNode
 
@@ -172,13 +174,16 @@ class AbstractFileReadOnly(ReprMixin):
         elif isinstance(file_path_or_raw, bytes):
             return function(*args, buffer=BytesIO(file_path_or_raw), **kwargs)
         elif isinstance(file_path_or_raw, str):
-            if file_path_or_raw.startswith(("http://", "https://")):
+            if file_path_or_raw.startswith("https://"):
                 parsed = parse.urlparse(file_path_or_raw)
-                url = parsed.scheme + "://" + parsed.netloc + parse.quote(parsed.path)
+                url = "https://" + parsed.netloc + parse.quote(parsed.path)
+                err = None
                 for i in range(4):
                     try:
                         return function(*args, buffer=request.urlopen(url), **kwargs)
                     except Exception as e:
+                        if err:
+                            console(f"Error {i} was: {err}", msg=Msg.error)
                         err = e
                         sleep(2**i)
                 raise err
