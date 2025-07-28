b3ba15673c26dfd104ba78c7b04a44433f67d36f
Andrew Beaumont <acbeaumo@gmail.com>
2025-07-28 19:39:22 -0400

    Dictionary

--- PyPoE/poe/file/translations.py
+++ PyPoE/poe/file/translations.py
@@ -2316,7 +2316,7 @@ def install_data_dependant_quantifiers(relational_reader: RelationalReader):
         id="mod_value_to_item_class",
         relational_reader=relational_reader,
         table="ItemClasses.dat64",
-        placeholder="<random item type>",
+        placeholder="&lt;random item type&gt;",
     )
 
     TQRelationalData(
@@ -2324,7 +2324,7 @@ def install_data_dependant_quantifiers(relational_reader: RelationalReader):
         relational_reader=relational_reader,
         table="Mods.dat64",
         predicate=("GenerationType", MOD_GENERATION_TYPE.TEMPEST),
-        placeholder="<random Tempest modifier>",
+        placeholder="&lt;random Tempest modifier&gt;",
     )
 
     if relational_reader.specification.sequel == 1:
@@ -2333,7 +2333,7 @@ def install_data_dependant_quantifiers(relational_reader: RelationalReader):
             relational_reader=relational_reader,
             table="IndexableSupportGems.dat64",
             index_column="Index",
-            placeholder="<random Support Gem>",
+            placeholder="&lt;random Support Gem&gt;",
         )
 
     TQRelationalData(
@@ -2348,7 +2348,7 @@ def install_data_dependant_quantifiers(relational_reader: RelationalReader):
             relational_reader=relational_reader,
             table="AfflictionRewardTypeVisuals.dat64",
             index_column="AfflictionRewardTypes",
-            placeholder="<Delirium reward>",
+            placeholder="&lt;Delirium reward&gt;",
         )
 
     if relational_reader.specification.sequel == 1:
@@ -2357,7 +2357,7 @@ def install_data_dependant_quantifiers(relational_reader: RelationalReader):
             relational_reader=relational_reader,
             table="IndexableSkillGems.dat64",
             index_column="Index",
-            placeholder="<Random Skill>",
+            placeholder="&lt;random Skill&gt;",
         )
 
     if relational_reader.specification.sequel == 2:
@@ -2385,7 +2385,7 @@ def install_data_dependant_quantifiers(relational_reader: RelationalReader):
         table="PassiveSkills.dat64",
         index_column="PassiveSkillGraphId",
         convert_type="short",
-        placeholder="<Passive Skill>",
+        placeholder="&lt;random Passive Skill&gt;",
     )
 
     TranslationQuantifierHandler.init()
